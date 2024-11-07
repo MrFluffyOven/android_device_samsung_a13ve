2adda4543f36afab7d416da3307552e117992909
Jamie Falconer <mrfluffyoven@gmail.com>
2024-11-07 10:34:18 +0000

    Write up health (attempted fastbootd fix)

--- device.mk
+++ device.mk
@@ -26,6 +26,12 @@ PRODUCT_SHIPPING_API_LEVEL := 31
 # Dynamic partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
+# Health
+PRODUCT_PACKAGES += \
+    android.hardware.health@2.1-impl \
+    android.hardware.health@2.1-impl.recovery \
+    android.hardware.health@2.1-service
+
 # fastbootd
 PRODUCT_PACKAGES += \
     android.hardware.fastboot@1.0-impl-mock \
